8996c8ce245b8f67690b7c66f26cc8a19b1d40a1
Andrei <andrei.massaini@hotmail.com>
2024-10-18 07:07:16 -0300

    Process in-situ XLSX to CSV, reformat parameters from rows to columns, and manually remove non-collected data.

---
+++ sitsu_data/format_sitsu_data.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import numpy as np
+
+""""
+Given the nature of the incorrectly formatted csv, the script below processes the data and formats the parameters from row to column.
+"""
+def process_csv(input_file, output_file):
+    df = pd.read_csv(input_file)
+
+    processed_data = {}
+
+    current_point = None
+    current_date = None
+    row_data = {}
+
+    for _, row in df.iterrows():
+        if row['PONTO'] != current_point or row['DATA'] != current_date:
+            if current_point is not None:
+                key = (current_point, current_date)
+                processed_data[key] = row_data
+            current_point = row['PONTO']
+            current_date = row['DATA']
+            row_data = {'PONTO': current_point, 'DATA': current_date}
+        
+        row_data[row['Parâmetro']] = row['RESULT.']
+        row_data[f"{row['Parâmetro']}_UN"] = row['UN.']
+
+    if current_point is not None:
+        key = (current_point, current_date)
+        processed_data[key] = row_data
+
+    new_df = pd.DataFrame.from_dict(processed_data, orient='index')
+    new_df.reset_index(drop=True, inplace=True)
+    columns = ['PONTO', 'DATA'] + sorted([col for col in new_df.columns if col not in ['PONTO', 'DATA']])
+    new_df = new_df[columns]
+
+    new_df.to_csv(output_file, index=False)
+
+    print(f"Processed file saved:  {output_file}")
+
+input_file = 'Base_kinross.csv'
+output_file = 'Base_kinross_processed.csv'
+process_csv(input_file, output_file)
